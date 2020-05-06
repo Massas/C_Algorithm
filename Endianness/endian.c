113273aec61581ee1bbadadf7a486ec6421768de
kJ <kajitam714@gmail.com>
2020-05-06 16:50:37 +0900

    add endian.c

---
+++ Endianness/endian.c
@@ -0,0 +1,24 @@
+/* endianness エンディアンネス */
+/* int型のエンディアンネスを調べる */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+    int i = 1;
+    
+    if(*((char *)&i))
+    {
+        printf("little-endian\n");
+    }
+    else if(*((char *)&i + (sizeof(int) - 1)))
+    {
+        printf("big-endian\n");
+    }
+    else
+    {
+        printf("不明\n");
+    }
+
+    return EXIT_SUCCESS;
+}
